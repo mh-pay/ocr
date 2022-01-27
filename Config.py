b3dec2fab8cfc5cf4d77a15bd3212fdf69b2fe1c
mh-pay <97784050+mh-pay@users.noreply.github.com>
2022-01-27 18:07:11 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
 import os
 
-ENVIRONMENT = bool(os.environ.get('ENVIRONMENT', False))
+ENVIRONMENT = os.environ.get('ENVIRONMENT', False)
 
 if ENVIRONMENT:
     try:
@@ -9,8 +9,19 @@ if ENVIRONMENT:
         raise Exception("Your API_ID is not a valid integer.")
     API_HASH = os.environ.get('API_HASH', None)
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
+    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
+    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
+    MUST_JOIN = os.environ.get('MUST_JOIN', None)
+    if MUST_JOIN.startswith("@"):
+        MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
     API_ID = 0
     API_HASH = ""
     BOT_TOKEN = ""
+    DATABASE_URL = ""
+    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
+    MUST_JOIN = ""
+    if MUST_JOIN.startswith("@"):
+        MUST_JOIN = MUST_JOIN[1:]
